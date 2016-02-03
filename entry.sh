f8178fb4b957e6c8d794a16b8d5ceefba3d11dc1
Greg <gwmullin@gmail.com>
2016-02-03 11:10:42 -0800

    ditch screen

--- entry.sh
+++ entry.sh
@@ -8,10 +8,10 @@ APACHE_PID=$!
 
 trap "{ kill $APACHE_PID ; echo 'Killed apache'; pkill -f php ; echo 'Killed php' ; sleep 10; kill $SCREEN_PID ; echo 'Killed screen' ; sleep 30; }"  SIGTERM EXIT
 
-cd /var/www/nZEDb/misc/update
+cd /var/www/nZEDb/misc/update/nix/screen/sequential
 
 while :
 do
-	screen -dmS UPDATER /var/www/nZEDb/misc/update/nix/screen/sequential/threaded.sh
+	./threaded.sh
 	sleep 60
 done
